6bd7f3d3763463ec0aa081a3d6c832039fcfb160
August Heggestad <theinnrlight@gmail.com>
2020-05-02 02:44:33 -0500

    future expansion
    Add files for eventual expantion to other source games we may want to mount without requiring the user to keep them installed.

--- installcsgo.ps1
+++ installcsgo.ps1
@@ -2,8 +2,8 @@ mkdir tmp\
 import-module bitstransfer
 Start-BitsTransfer "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip" ".\tmp\steamcmd.zip"
 Expand-Archive -Path '.\tmp\steamcmd.zip' -DestinationPath .\
-robocopy ".\" steamcmd\ steamcmd.exe momcss.txt
-.\steamcmd\steamcmd.exe +runscript momcss.txt
+robocopy ".\" steamcmd\ steamcmd.exe momcsgo.txt
+.\steamcmd\steamcmd.exe +runscript momcsgo.txt
 .\steamcmd\tmp\bin\vpk.exe .\steamcmd\tmp\cstrike\cstrike_pak_dir.vpk
 robocopy "steamcmd\tmp\cstrike\cstrike_pak_dir\materials" "css\materials" /MIR
 robocopy "steamcmd\tmp\cstrike\cstrike_pak_dir\models" "%css\models" /MIR
--- installtf2.ps1
+++ installtf2.ps1
@@ -2,8 +2,9 @@ mkdir tmp\
 import-module bitstransfer
 Start-BitsTransfer "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip" ".\tmp\steamcmd.zip"
 Expand-Archive -Path '.\tmp\steamcmd.zip' -DestinationPath .\
-robocopy ".\" steamcmd\ steamcmd.exe momcss.txt
-.\steamcmd\steamcmd.exe +runscript momcss.txt
+robocopy ".\" steamcmd\ steamcmd.exe momtf2.txt
+.\steamcmd\steamcmd.exe +runscript momtf2.txt
+pause
 .\steamcmd\tmp\bin\vpk.exe .\steamcmd\tmp\cstrike\cstrike_pak_dir.vpk
 robocopy "steamcmd\tmp\cstrike\cstrike_pak_dir\materials" "css\materials" /MIR
 robocopy "steamcmd\tmp\cstrike\cstrike_pak_dir\models" "%css\models" /MIR
